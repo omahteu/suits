593564c4f6d60e18763ae694ca5d36094fc8fa33
matheus <matheus.mmn@hotmail.com>
2023-09-25 12:47:26 -0300

    ajustes

--- js/caixa/manutencao.js
+++ js/caixa/manutencao.js
@@ -3,6 +3,6 @@ import salvar from "../olivia/salva.js"
 import {RAIZ} from "../raiz.js"
 
 export default function envia_dados_manutencao(caixa, data, hora, suite, motivo, duracao) {
-    var dados = 'caixa='+ caixa, '&data='+ data, '&hora='+ hora, '&suite='+ suite, '&motivo='+ motivo, '&tempo='+ duracao
+    var dados = 'caixa='+ caixa+ '&data='+ data+ '&hora='+ hora+ '&suite='+ suite+ '&motivo='+ motivo+ '&tempo='+ duracao
     salvar(`http://${RAIZ}/suits/php/suites/dadosmanutencao.php`, dados)
 }
