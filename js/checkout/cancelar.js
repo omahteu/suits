a3213fb38b93a5586559d20be5cfb3bb40d8d93d
matheus <matheus.mmn@hotmail.com>
2023-09-30 16:45:10 -0300

    cancelar locacao

---
+++ js/checkout/cancelar.js
@@ -0,0 +1,10 @@
+import alterar from "../olivia/altera.js"
+import {RAIZ} from "../raiz.js"
+
+
+$(document).on("click", "#cancel", function() {
+    var suite = localStorage.getItem('last')
+    var dados = 'suite=' + suite + '&tipo=' + 'locado'
+    alterar(`http://${RAIZ}/suits/php/suites/editarinfosq.php`, dados)
+    setTimeout(() => { window.close() }, 1000)
+})
