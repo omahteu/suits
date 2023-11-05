6f347f45b324e4916bfdb26f5c301f7164c2b4f9
Matheus Martins <matheus.mmn@hotmail.com>
2023-11-05 20:55:21 -0300

    ajustes

--- js/quartos/calculos/inicial.js
+++ js/quartos/calculos/inicial.js
@@ -4,8 +4,9 @@ export function valoresIniciais(quarto) {
     var i1 = receber("dados_suites")
     var i2 = receber("tabela_precos")
     var infosQuatos = i1.filter(e => e.numeroSuite == quarto)
+    var infosTabela = i2.filter(z => z.codigo == infosQuatos[0].codigoSuite)
     var horas_iniciais_locacao = infosQuatos[0].horas_locacaoSuite
-    var valor_inicial_locacao = i2[0].locacao
+    var valor_inicial_locacao = infosTabela[0].locacao
     var valorInicial = Number(valor_inicial_locacao) * Number(horas_iniciais_locacao)
     sessionStorage.setItem("valorInicialSuite", valorInicial)
 }
