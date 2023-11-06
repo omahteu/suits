d48a5a646bb34d68ea1e726b346132132fc331a0
Matheus Martins <matheus.mmn@hotmail.com>
2023-11-06 10:24:54 -0300

    ajustes

--- js/suites/conta/adicional.js
+++ js/suites/conta/adicional.js
@@ -6,23 +6,23 @@ $(document).on('click', '[class="card"]', function () {
 	let r3 = $(r2[0].children[1])
 	let suite = r3.text()
     setTimeout(() => {
-
+		let quarto = $("#vq_painel").text()
+		let valor = 0
+		calculo(suite, quarto, valor)
 	}, 500);
-	let quarto = $("#vq_painel").text()
-	let valor = 0
-	calculo(suite, quarto, valor)
 })
 
 async function calculo(suite, quarto,  valor) {
+	let offs = JSON.parse(sessionStorage.getItem('offs'))
 	const rq = await fetch(`http://${RAIZ}/suits/php/suites/show/cofre.php`)
 	const rs = await rq.json()
 	if (rs["status"]) {
 		let ficha = rs["dados"].filter(i => i.suite == suite)
+		let aberto = offs.filter(z => z.suite == suite)
 		ficha.forEach(el => {
 			valor += parseFloat(el.valor)
 		});
-		let adicionado = parseFloat(valor) - parseFloat(quarto)
-		
+		let adicionado = parseFloat(valor) - parseFloat(aberto[0].valor)
 		$("#vh_painel").text(parseFloat(adicionado).toFixed(2))
 	}
 }
