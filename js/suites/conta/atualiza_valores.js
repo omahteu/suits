c42bf2466a4d8666f77c36e1317532f52abcb1f0
Matheus Martins <matheus.mmn@hotmail.com>
2023-11-07 08:46:32 -0300

    ajustes

--- js/suites/conta/atualiza_valores.js
+++ js/suites/conta/atualiza_valores.js
@@ -26,31 +26,36 @@ function atualizaValores(suite) {
     if (modo_cobranca == "hora") {
       let funil = alugados.filter(x => x.suite == suite)
       let iniciado = funil[0].hora
-      const horarioRegistrado = moment(iniciado, 'HH:mm:ss');
       let funil_suites = suites.filter(o => o.numeroSuite == funil[0].suite)
       let funil_precos = precos.filter(u => u.codigo == funil_suites[0].codigoSuite)
       function verificarHoraPassada() {
         const horaAtual = moment();
-        const diferencaEmHoras = horaAtual.diff(horarioRegistrado, 'hours')
+        const horaInicial = moment(String(iniciado), 'HH:mm:ss');
 
-        const diferencaSegundos = horaAtual.diff(horarioRegistrado, 'seconds');
-        const minutos = Math.floor((diferencaSegundos % 3600) / 60);
+        if (horaAtual.isBefore(horaInicial)) {
+            horaAtual.add(1, 'day');
+        }
 
-        if (diferencaEmHoras > 0 && diferencaEmHoras <= 1 && minutos > tolerancia) {
+        const diferenca = moment.duration(horaAtual.diff(horaInicial));
+
+        const horasDiferenca = diferenca.hours();
+        const minutosDiferenca = diferenca.minutes();
+
+        if (horasDiferenca > 0 && horasDiferenca <= 1 && minutosDiferenca > tolerancia) {
           alterarValor(suite, funil_precos[0].vh2)
-        } else if (diferencaEmHoras > 1 && diferencaEmHoras <= 2 && minutos > tolerancia) {
+        } else if (horasDiferenca > 1 && horasDiferenca <= 2 && minutosDiferenca > tolerancia) {
           alterarValor(suite, funil_precos[0].vh3)
-        } else if(diferencaEmHoras > 2 && diferencaEmHoras <= 3 && minutos > tolerancia) {
+        } else if(horasDiferenca > 2 && horasDiferenca <= 3 && minutosDiferenca > tolerancia) {
           alterarValor(suite, funil_precos[0].vh4)
-        } else if (diferencaEmHoras > 3 && diferencaEmHoras <= 4 && minutos > tolerancia) {
+        } else if (horasDiferenca > 3 && horasDiferenca <= 4 && minutosDiferenca > tolerancia) {
           alterarValor(suite, funil_precos[0].vh5)
-        } else if (diferencaEmHoras > 4 && diferencaEmHoras <= 5 && minutos > tolerancia) {
+        } else if (horasDiferenca > 4 && horasDiferenca <= 5 && minutosDiferenca > tolerancia) {
           alterarValor(suite, funil_precos[0].vh6)
-        } else if (diferencaEmHoras > 5 && diferencaEmHoras <= 6 && minutos > tolerancia) {
+        } else if (horasDiferenca > 5 && horasDiferenca <= 6 && minutosDiferenca > tolerancia) {
           alterarValor(suite, parseFloat(parseInt(funil_precos[0].vh6) + 10).toFixed(2))
-        } else if (diferencaEmHoras > 6 && diferencaEmHoras <= 7 && minutos > tolerancia) {
+        } else if (horasDiferenca > 6 && horasDiferenca <= 7 && minutosDiferenca > tolerancia) {
           alterarValor(suite, parseFloat(parseInt(funil_precos[0].vh6) + 20).toFixed(2))
-        } else if (diferencaEmHoras > 7 && diferencaEmHoras <= 8 && minutos > tolerancia) {
+        } else if (horasDiferenca > 7 && minutosDiferenca > tolerancia) {
           comecando_pernoite()
         }
       }
