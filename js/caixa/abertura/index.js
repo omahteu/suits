9cb50945309add9161ddab24cbc59aec593ab119
Matheus Martins <matheus.mmn@hotmail.com>
2023-11-13 15:54:46 -0300

    ajustes

--- js/caixa/abertura/index.js
+++ js/caixa/abertura/index.js
@@ -1,8 +1,7 @@
-// import { data_atual } from "../../geradores/data.js"
-// import { hora_atual_segundos } from "../../geradores/hora.js"
-// import link from "../../setup/index.js"
-// import salvar from "../../olivia/salva.js"
-// import {RAIZ} from "../../raiz.js"
+import { data_atual } from "../../geradores/data.js"
+import { hora_atual_segundos } from "../../geradores/hora.js"
+import salvar from "../../olivia/salva.js"
+import {RAIZ} from "../../raiz.js"
 
 $(document).on("change", "#usarFundoCaixa", function(){
     let tipo = $("#usarFundoCaixa :selected").text()
@@ -16,7 +15,7 @@ $(document).on("change", "#usarFundoCaixa", function(){
 $(document).on("click", "#abrirCaixa", function() {
     var utilizar_fundo_caixa = $("#usarFundoCaixa").val()
     var usar_produtos_sem_caixa = $("#usarProdutosSemCaixa").val()
-    // var usuario = $("#usuario").val()
+    var usuario = $("#usuario").val()
     var fundoCaixa = $("#valorFundoCaixa").val()
     var fundoCaixa2 = "0"
 
@@ -30,8 +29,8 @@ $(document).on("click", "#abrirCaixa", function() {
         localStorage.setItem("prod", usar_produtos_sem_caixa)     
         var fundoCaixa_formatado = String(fundoCaixa).replace(",", ".")
         localStorage.setItem('fundo', fundoCaixa_formatado)
-        // let dados = 'data=' + data_atual() + '&entrada=' + hora_atual_segundos() + '&usuario=' + usuario + '&fundo=' + fundoCaixa_formatado + '&total=' + '' + '&saida=' + ''
-        // salvar(`http://${RAIZ}/suits/php/suites/caixa.php`, dados)
+        let dados = 'data=' + data_atual() + '&entrada=' + hora_atual_segundos() + '&usuario=' + usuario + '&fundo=' + fundoCaixa_formatado + '&total=' + '' + '&saida=' + ''
+        salvar(`http://${RAIZ}/suits/php/suites/caixa.php`, dados)
         alert("Caixa Aberto!")
         $(location).attr('href', '../html/home.html')
     } else if (utilizar_fundo_caixa == "sim" && usar_produtos_sem_caixa == "nao") {
@@ -39,8 +38,8 @@ $(document).on("click", "#abrirCaixa", function() {
         localStorage.setItem("prod", usar_produtos_sem_caixa)
         var fundoCaixa_formatado = String(fundoCaixa).replace(",", ".")
         localStorage.setItem('fundo', fundoCaixa_formatado)
-        // let dados = 'data=' + data_atual() + '&entrada=' + hora_atual_segundos() + '&usuario=' + usuario + '&fundo=' + fundoCaixa_formatado + '&total=' + '' + '&saida=' + ''
-        // salvar(`http://${RAIZ}/suits/php/suites/caixa.php`, dados)
+        let dados = 'data=' + data_atual() + '&entrada=' + hora_atual_segundos() + '&usuario=' + usuario + '&fundo=' + fundoCaixa_formatado + '&total=' + '' + '&saida=' + ''
+        salvar(`http://${RAIZ}/suits/php/suites/caixa.php`, dados)
         alert("Caixa Aberto!")
         $(location).attr('href', '../html/home.html')
     } else if (utilizar_fundo_caixa == "nao" && usar_produtos_sem_caixa == "sim") {
@@ -48,8 +47,8 @@ $(document).on("click", "#abrirCaixa", function() {
         localStorage.setItem("prod", usar_produtos_sem_caixa)
         var fundoCaixa_formatado = String(fundoCaixa2).replace(",", ".")
         localStorage.setItem('fundo', fundoCaixa_formatado)
-        // let dados = 'data=' + data_atual() + '&entrada=' + hora_atual_segundos() + '&usuario=' + usuario + '&fundo=' + fundoCaixa_formatado + '&total=' + '' + '&saida=' + ''
-        // salvar(`http://${RAIZ}/suits/php/suites/caixa.php`, dados)
+        let dados = 'data=' + data_atual() + '&entrada=' + hora_atual_segundos() + '&usuario=' + usuario + '&fundo=' + fundoCaixa_formatado + '&total=' + '' + '&saida=' + ''
+        salvar(`http://${RAIZ}/suits/php/suites/caixa.php`, dados)
         alert("Caixa Aberto!")
         $(location).attr('href', '../html/home.html')
     } else if (utilizar_fundo_caixa == "nao" && usar_produtos_sem_caixa == "nao") {
@@ -57,8 +56,8 @@ $(document).on("click", "#abrirCaixa", function() {
         localStorage.setItem("prod", usar_produtos_sem_caixa)
         var fundoCaixa_formatado = String(fundoCaixa2).replace(",", ".")
         localStorage.setItem('fundo', fundoCaixa_formatado)
-        // let dados = 'data=' + data_atual() + '&entrada=' + hora_atual_segundos() + '&usuario=' + usuario + '&fundo=' + fundoCaixa_formatado + '&total=' + '' + '&saida=' + ''
-        // salvar(`http://${RAIZ}/suits/php/suites/caixa.php`, dados)
+        let dados = 'data=' + data_atual() + '&entrada=' + hora_atual_segundos() + '&usuario=' + usuario + '&fundo=' + fundoCaixa_formatado + '&total=' + '' + '&saida=' + ''
+        salvar(`http://${RAIZ}/suits/php/suites/caixa.php`, dados)
         alert("Caixa Aberto!")
         $(location).attr('href', '../html/home.html')
     }
